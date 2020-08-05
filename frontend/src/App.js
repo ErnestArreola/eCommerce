5d8e4afaba1a0676c8e4c55244348009b59c98d1
ErnestArreola <arreern@gmail.com>
2020-08-04 17:17:50 -0700

    React App

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+import {Button} from 'reactstrap';
+//components 
+import IndexNav from "./components/Navbar/IndexNavbar.js";
+
+function App() {
+  return (
+
+    <div className="App">
+   <IndexNav></IndexNav>
+    </div>
+  );
+}
+
+export default App;
