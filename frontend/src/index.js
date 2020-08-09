5219cc60550fe6eb147fd6abcc15d21fb43bc254
ErnestArreola <arreern@gmail.com>
2020-08-09 12:24:38 -0700

    Fixes missing files bug

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import store from './store';
 
-// styles
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import "./assets/css/style.css";
-import "./assets/scss/global.scss";
+
+//style 
+import './index.css';
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  </Provider>, document.getElementById('root'));
